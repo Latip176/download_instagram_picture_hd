9df46dc5f3222a128c028e0110cbb7b03c65b40d
Muhammad Latif Harkat <68332606+Latip176@users.noreply.github.com>
2022-01-12 13:32:11 +0700

    Add files via upload

---
+++ instagram_picture_hd.py
@@ -0,0 +1,47 @@
+import requests as req, re
+
+class Main:
+	
+	def __init__(
+		self,username
+	):
+		self.username = username
+	def getProfile(
+		self
+	):
+		r = req.get(
+			f"https://instagram.com/{self.username}/?__a=1"
+		).text
+		try:
+			_link = re.findall(
+				'\"profile_pic_url_hd\"\:\"(.*?)\"',str(
+					r
+				)
+			)[0]
+		except KeyError:
+			exit(
+				"[!] Username tidak ditemukan."
+			)
+		with open(
+			f"/sdcard/download/profile_{self.username}.jpg", "wb"
+		) as f:
+			__get_picture_hd = req.get(
+				_link
+			).content
+			f.write(
+				__get_picture_hd
+			)
+		return f"[✓] Foto disimpan di folder download\n * Nama file: profile_{self.username}.jpg "
+
+if __name__=="__main__":
+	print(
+		" * Download profile picture Instagram HD\n"
+	)
+	_username = input(
+		"[+] Username Instagram target: "
+	)
+	print(
+		Main(
+			_username
+		).getProfile()
+	)
